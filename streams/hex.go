dd231da6bd24c2cd2eb55bf754070e599ebfa1df
Javier Peletier <jm@epiclabs.io>
2019-03-28 00:52:55 +0100

    hex exercise responses

--- streams/hex.go
+++ streams/hex.go
@@ -1,6 +1,8 @@
 package streams
 
 import (
+	"bytes"
+	"encoding/hex"
 	"errors"
 	"io"
 )
@@ -9,23 +11,50 @@ var ErrInvalidHexChar = errors.New("Invalid character in hex stream")
 var ErrMissingHexChar = errors.New("Hex stream contains an odd number of characters")
 
 type HexReader struct {
+	ir io.Reader
 }
 
 func NewHexReader(r io.Reader) io.Reader {
-	return nil
+	return &HexReader{
+		ir: r,
+	}
 }
 
 func (r *HexReader) Read(p []byte) (int, error) {
-	return 0, nil
+	d := make([]byte, len(p)*2)
+	i, err := r.ir.Read(d)
+	if err != nil {
+		return i, err
+	}
+
+	if i%2 != 0 {
+		return i, ErrMissingHexChar
+	}
+
+	d = d[:i]
+	n, err := hex.Decode(p, d)
+	if err != nil {
+		err = ErrInvalidHexChar
+	}
+	return n, err
 }
 
 type HexWriter struct {
+	iw io.Writer
 }
 
 func NewHexWriter(w io.Writer) io.Writer {
-	return nil
+	return &HexWriter{
+		iw: w,
+	}
 }
 
 func (w *HexWriter) Write(p []byte) (int, error) {
-	return 0, nil
+	d := make([]byte, len(p)*2)
+	hex.Encode(d, p)
+	_, err := w.iw.Write(bytes.ToUpper(d))
+	if err != nil {
+		return 0, err
+	}
+	return len(p), nil
 }
